a82f0fdb2944c5ebca58e5e395e3ee01978f28ca
BharathKarkera <94306701+BharathKarkera@users.noreply.github.com>
2023-02-14 13:43:41 -0600

    Update loader.sh

--- loader.sh
+++ loader.sh
@@ -1,5 +1,8 @@
 #! /bin/bash 
 
+curl -vi "http://localhost:8983/solr/admin/cores?action=UNLOAD&deleteInstanceDir=true&core=search_engine_collection"
+
+
 /usr/local/bin/python3 << EOF 
 
 import csv 
@@ -10,5 +13,6 @@ with open("/Users/bharathraj.karkera/python_test/Solr_based_searchZ_engine-main/
 
 EOF
 
+solr create -c search_engine_collection -p 8983
 
 curl -i 'http://localhost:8983/solr/search_engine_collection/update?commit=true' --data-binary @/Users/bharathraj.karkera/python_test/Solr_based_searchZ_engine-main/document.csv -H 'Content-type:application/csv'
